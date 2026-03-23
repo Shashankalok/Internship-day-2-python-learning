e70ab3d3fabe90fdc18aefc949cf5beb3b6380a3
Shashank Alok <101627380+Shashankalok@users.noreply.github.com>
2026-03-23 18:01:54 +0530

    15 small python programs
    15 small programs covering:
    Variables and data types
    If-else conditions
    For/while loops
    Functions with parameters
    Lists and dictionaries manipulation

---
+++ # 1_variables.py
@@ -0,0 +1,21 @@
+# 1_variables.py
+a = 10
+b = 9.7
+name = "Shashank"
+
+print(a, b, name)
+
+# 2_datatypes.py
+x = 10
+y = "Shashank"
+print(type(x))
+print(type(y))
+
+# 3_even_odd.py
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
